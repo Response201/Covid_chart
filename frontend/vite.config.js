c583f4b69f619026bce3de6c0e1ff02cee7b04df
jess <jessica.response201@gmail.com>
2022-12-05 18:19:21 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+const Dotenv = require('dotenv-webpack');
 
 // https://vitejs.dev/config/
 export default defineConfig({
