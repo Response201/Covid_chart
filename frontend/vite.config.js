aa38a1b49202d540af32cf67c96d2549cbfb9794
jess <jessica.response201@gmail.com>
2022-12-05 18:40:12 +0100

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const Dotenv = require('dotenv-webpack');
+const mode = process.env.APP_ENV
 
 // https://vitejs.dev/config/
 export default defineConfig({
+ 
   plugins: [react(),
-    new Dotenv({
-      safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
-      allowEmptyValues: true, // allow empty variables (e.g. `FOO=`) (treat it as empty string, rather than missing)
-      systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
-      silent: true, // hide any errors
-      prefix: 'import.meta.env.'
-  })],
+   ],
   
 
 
